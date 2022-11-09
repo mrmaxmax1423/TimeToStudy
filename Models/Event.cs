899ef50284a97dbeb1f72c8e161251de147f8546
mrmaxmax1423 <maximilianods@ymail.com>
2022-11-09 15:29:22 -0500

    Created Event Model
    created event model and started adding input to the add event page

---
+++ Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeToStudy.Models
+{
+    public class Event
+    {
+        [Required(ErrorMessage = "Please enter a name for the event.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description for the event.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter a due date.")]
+        public DateTime? DueDate { get; set; }
+    }
+}
