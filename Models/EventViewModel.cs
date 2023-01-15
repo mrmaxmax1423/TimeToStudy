d9487f564d6dbd96de8972f1d9aca19b920b2d95
mrmaxmax1423 <maximilianods@ymail.com>
2023-01-15 09:19:19 -0500

    added model and controller for Event
    Removed unnecessary files, added model and controller for events and reworked view

---
+++ Models/EventViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeToStudy.Models
+{
+    public class EventViewModel
+    {
+        public string EventLabel { get; set; }
+        public string EventDescription { get; set; }
+        public double EventLength { get; set; }
+        public bool Reccuring { get; set; }
+        public bool SetTime { get; set; }
+        public DateTime EventTime { get; set; }
+    }
+
+    /*
+    public EventViewModel AddEvent()
+    {
+        string EventName = ;
+    }
+    */
+}
